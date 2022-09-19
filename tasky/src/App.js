1ac6e748ac7b94628bc57372386c0af6eb3c45f3
Shunyi Xu <3051778755@qq.com>
2022-09-19 19:14:39 +0800

    Completed Week 2 Lab B

--- tasky/src/App.js
+++ tasky/src/App.js
@@ -1,15 +1,28 @@
-
+import React, {useState} from 'react';
 import './App.css';
 import Task from './components/Task';
 
 function App() {
+  const [ taskState, setTaskState ] = useState({
+    tasks: [
+      { id: 1, title: "Dishes", description: "Empty dishwasher", deadline: "Today" },
+      { id: 2, title: "Laundry", description: "Fold clothes and put away", deadline: "Tomorrow" },
+      { id: 3, title: "Tidy up", deadline: "Today" },
+      { id: 4, title: "Go shopping", description:"Buy some meat and fruit", deadline: "The day after tomorrow"},
+      { id: 5, title: "finall year project", description:"A game", deadline:"School year end"}
+    ]
+  });
   return (
-    
     <div className="container">
       <h1>Tasky</h1>
-      <Task title="Dishes" deadline="Today" description="Empty dishwasher"/>
-      <Task title="Laundry" deadline="Tomorrow"  description="Fold laundry and put away"/>
-      <Task title="Tidy" deadline="Today" />
+      {taskState.tasks.map((task) => (              
+        <Task 
+          title={task.title}
+          description={task.description}
+          deadline={task.deadline}
+          key={task.id}
+        />
+      ))} 
     </div>
   );
 }
